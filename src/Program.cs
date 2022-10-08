ce7a08e027fd0e6500d3e0aee9c1adc44d18b41a
OnemanWolf <timothy.oleson@gmail.com>
2022-10-08 18:35:01 -0400

    first commit

---
+++ src/Program.cs
@@ -0,0 +1,35 @@
+﻿using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using Newtonsoft.Json.Bson;
+
+namespace JTokenExist;
+class Program
+{
+    static void Main(string[] args)
+    {
+
+        var jpath = @"$.MemeberId";
+        var json = @"{""MemeberId"":""123""}";
+
+        JToken jToken = JToken.Parse(json);
+
+        JToken? patientId = GetPatientId(jpath, jToken);
+        //  var value = jToken.Value<object>(); // "b"
+        if((string)patientId != ""){
+          Console.WriteLine(patientId);
+        }
+        else{
+            Console.WriteLine("Not found");
+        }
+
+
+
+        
+    }
+
+    private static JToken? GetPatientId(string jpath, JToken jToken)
+    {
+            return  jToken.Any() ? jToken.SelectToken(jpath) :  null; // true
+
+    }
+}
